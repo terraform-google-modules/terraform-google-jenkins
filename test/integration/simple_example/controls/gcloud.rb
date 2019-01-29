9a22d584b7a613f6eb90e3c5e5c89bbf2c0d4381
Jason Berlinsky <jason@barefootcoders.com>
2019-01-28 22:32:28 -0500

    Add test coverage for firewall rules and IAM bindings

--- test/integration/simple_example/controls/gcloud.rb
+++ test/integration/simple_example/controls/gcloud.rb
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 project_id = attribute('project_id')
-region = attribute('region')
 jenkins_instance_zone = attribute('jenkins_instance_zone')
 jenkins_instance_name = attribute('jenkins_instance_name')
 
@@ -24,5 +23,61 @@ control "gcloud" do
     it { should exist }
     its('machine_type') { should end_with "n1-standard-4" }
     its('status') { should eq 'RUNNING' }
+  end
+
+  describe google_compute_firewall(project: project_id, name: "jenkins-#{jenkins_instance_name}-external-tcp-80") do
+    it { should exist }
+    its(:allowed_http?) { should be true }
+    its(:direction) { should eq "INGRESS" }
+    its(:source_ranges) { should eq ["0.0.0.0/0"] }
+    it { should allow_target_tags_only ["jenkins"] }
+  end
+
+  describe google_compute_firewall(project: project_id, name: "jenkins-#{jenkins_instance_name}-external-tcp-443") do
+    it { should exist }
+    its(:allowed_https?) { should be true }
+    its(:direction) { should eq "INGRESS" }
+    its(:source_ranges) { should eq ["0.0.0.0/0"] }
+    it { should allow_target_tags_only ["jenkins"] }
+  end
+
+  describe google_service_account(name: "projects/#{project_id}/serviceAccounts/jenkins@#{project_id}.iam.gserviceaccount.com") do
+    it { should exist }
+    its(:display_name) { should eq "Jenkins" }
+  end
+
+  describe google_project_iam_binding(project: project_id, role: "roles/compute.instanceAdmin.v1") do
+    it { should exist }
+    its(:members) { should include "serviceAccount:jenkins@#{project_id}.iam.gserviceaccount.com" }
+  end
+
+  describe google_project_iam_binding(project: project_id, role: "roles/compute.instanceAdmin") do
+    it { should exist }
+    its(:members) { should include "serviceAccount:jenkins@#{project_id}.iam.gserviceaccount.com" }
+  end
+
+  describe google_project_iam_binding(project: project_id, role: "roles/compute.networkAdmin") do
+    it { should exist }
+    its(:members) { should include "serviceAccount:jenkins@#{project_id}.iam.gserviceaccount.com" }
+  end
+
+  describe google_project_iam_binding(project: project_id, role: "roles/compute.securityAdmin") do
+    it { should exist }
+    its(:members) { should include "serviceAccount:jenkins@#{project_id}.iam.gserviceaccount.com" }
+  end
+
+  describe google_project_iam_binding(project: project_id, role: "roles/iam.serviceAccountActor") do
+    it { should exist }
+    its(:members) { should include "serviceAccount:jenkins@#{project_id}.iam.gserviceaccount.com" }
+  end
+
+  describe google_project_iam_binding(project: project_id, role: "roles/iam.serviceAccountUser") do
+    it { should exist }
+    its(:members) { should include "serviceAccount:jenkins@#{project_id}.iam.gserviceaccount.com" }
+  end
+
+  describe google_storage_bucket_iam_binding(bucket: "#{project_id}-jenkins-artifacts", role: "roles/storage.admin") do
+    it { should exist }
+    its(:members) { should include "serviceAccount:jenkins@#{project_id}.iam.gserviceaccount.com" }
   end
 end
