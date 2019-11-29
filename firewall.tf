9c07e59f276cb57552e63d7df209ba8de9a33916
Bohdan Yurov <bohdan@yurov.me>
2019-11-29 16:59:06 +0200

    Fixes #2: Pull useful example code into submodule https://github.com/terraform-google-modules/terraform-google-jenkins/issues/2
    Added submodule for artifacts.

--- firewall.tf
+++ firewall.tf
@@ -42,3 +42,56 @@ resource "google_compute_firewall" "jenkins-external-443" {
   target_service_accounts = [google_service_account.jenkins.email]
 }
 
+resource "google_compute_firewall" "jenkins_agent_ssh_from_instance" {
+  count = var.create_firewall_rules ? 1 : 0
+
+  name    = "jenkins-agent-ssh-access"
+  network = var.jenkins_workers_network
+  project = var.project_id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_tags = var.jenkins_instance_tags
+  target_tags = var.jenkins_workers_network_tags
+}
+
+resource "google_compute_firewall" "jenkins_agent_discovery_from_agent" {
+  count = var.create_firewall_rules ? 1 : 0
+
+  name    = "jenkins-agent-udp-discovery"
+  network = var.jenkins_instance_network
+  project = coalesce(var.jenkins_network_project_id, var.project_id)
+
+  allow {
+    protocol = "udp"
+  }
+
+  allow {
+    protocol = "tcp"
+  }
+
+  source_tags = concat(var.jenkins_instance_tags, var.jenkins_workers_network_tags)
+  target_tags = concat(var.jenkins_instance_tags, var.jenkins_workers_network_tags)
+}
+
+resource "google_compute_firewall" "jenkins_agent_discovery_from_agent_workers" {
+  count = var.create_firewall_rules ? 1 : 0
+
+  name    = "jenkins-agent-udp-discovery-workers"
+  network = var.jenkins_workers_network
+  project = coalesce(var.jenkins_network_project_id, var.project_id)
+
+  allow {
+    protocol = "udp"
+  }
+
+  allow {
+    protocol = "tcp"
+  }
+
+  source_tags = concat(var.jenkins_instance_tags, var.jenkins_workers_network_tags)
+  target_tags = concat(var.jenkins_instance_tags, var.jenkins_workers_network_tags)
+}
